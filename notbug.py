589a6bf9703794dfc7df2cc16cbd761a84c5d7b1
michal-szastok <87010859+michal-szastok@users.noreply.github.com>
2022-03-26 13:39:47 +0100

    Update notbug.py

--- notbug.py
+++ notbug.py
@@ -10,15 +10,11 @@ print(f"Name(s) of user(s) from Poland: \n {users_from_poland} \n")
 # Display sum of first ten elements starting from element 5:
 # numbers = [1,5,2,3,1,4,1,23,12,2,3,1,2,31,23,1,2,3,1,23,1,2,3,123]
 numbers = [1, 5, 2, 3, 1, 4, 1, 23, 12, 2, 3, 1, 2, 31, 23, 1, 2, 3, 1, 23, 1, 2, 3, 123]
-sum_of_10_elements_starting_with_5 = 0
-for n in range(numbers.index(5), numbers.index(5)+9):
-    sum_of_10_elements_starting_with_5 += n
+sum_of_10_elements_starting_with_5 = sum(numbers[5:15])
 print(f"Sum of first ten elements starting from element 5: \n {sum_of_10_elements_starting_with_5} \n")
 
 
 # Fill list with powers of 2, n [1..20]
-squares = []
-for n in range(1, 21):
-    squares.append(n ** 2)
-print(f"List of powers of 2, n [1..20]: \n"
+squares = [2 ** n for n in range(0, 20)]
+print(f"List 2^n [1..20]: \n"
       f"{squares}")
